ad9c17d91b9bd4d59f10355c7a271f7e36129e9d
“Haorong <li000592@algonquinlive.com>
2020-11-19 01:55:17 -0500

    fix a bug when touching different button at the same time

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,21 +9,24 @@ function Calculator() {
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     if (wasClickedOrder.length > 0) {
       const finalNumber = wasClickedOrder.reduce(reducer);
-      console.log(wasClickedOrder.length);
       setDisplayNumber(finalNumber);
+    } else {
+      setDisplayNumber(0);
     }
-    console.log("wasClickedOrder===>", wasClickedOrder);
+    console.log(wasClickedOrder);
+    console.log(displayNumber);
   }, [wasClickedOrder]);
 
   const clickKeyboard = (ev) => {
-    const clickedNumber = ev.target.innerText;
+    const clickedKeyboard = ev.target.innerText;
+    if (clickedKeyboard.length > 1)
+      return console.warn("Illegal interaction: muti number coming");
 
-    if (clickedNumber === "X") {
+    if (clickedKeyboard === "X") {
       setWasClickedOrder((currentArray) => {
         return currentArray.slice(0, currentArray.length - 1);
       });
     } else {
-      console.log(wasClickedOrder.indexOf("."));
       if (
         wasClickedOrder.includes(".") &&
         wasClickedOrder.length - wasClickedOrder.indexOf(".") > 2
@@ -32,37 +35,37 @@ function Calculator() {
         return;
       } else {
         setWasClickedOrder((currentArray) =>
-          currentArray.concat(clickedNumber)
+          currentArray.concat(clickedKeyboard)
         );
       }
     }
   };
   return (
-    <div>
+    <div className="numberKeyboard">
       <div className="displayNumber">
         <h1>{displayNumber}</h1>
       </div>
 
       <div className="keyboard" onClick={clickKeyboard}>
-        <div className="1stRow row">
-          <div className="1stColumn btn ">1</div>
-          <div className="2ndColumn btn">2</div>
-          <div className="3rdColumn btn">3</div>
+        <div className="row">
+          <div className="btn ">1</div>
+          <div className="btn">2</div>
+          <div className="btn">3</div>
         </div>
-        <div className="2ndRow row">
-          <div className="1stColumn btn">4</div>
-          <div className="2ndColumn btn">5</div>
-          <div className="3rdColumn btn">6</div>
+        <div className="row">
+          <div className="btn">4</div>
+          <div className="btn">5</div>
+          <div className="btn">6</div>
         </div>
-        <div className="3rdRow row">
-          <div className="1stColumn btn">7</div>
-          <div className="2ndColumn btn">8</div>
-          <div className="3rdColumn btn">9</div>
+        <div className="row">
+          <div className="btn">7</div>
+          <div className="btn">8</div>
+          <div className="btn">9</div>
         </div>
-        <div className="4thRow row">
-          <div className="1stColumn btn">0</div>
-          <div className="2ndColumn btn">.</div>
-          <div className="3rdColumn btn">X</div>
+        <div className="row">
+          <div className="btn">0</div>
+          <div className="btn">.</div>
+          <div className="btn">X</div>
         </div>
       </div>
     </div>
