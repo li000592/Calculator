92081e585acb4a06b57dd21bf484513fbdff8310
“Haorong <li000592@algonquinlive.com>
2020-11-19 01:26:21 -0500

    fix some bugs about display

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,28 +3,39 @@ import "./Calculator.css";
 
 function Calculator() {
   const [displayNumber, setDisplayNumber] = React.useState(0);
-  const [wasClickedOrder, setWasClickedOrder] = React.useState([""]);
+  const [wasClickedOrder, setWasClickedOrder] = React.useState([]);
 
   React.useEffect(() => {
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    const finalNumber = wasClickedOrder.reduce(reducer);
-    console.log(wasClickedOrder.length);
-    setDisplayNumber(finalNumber);
+    if (wasClickedOrder.length > 0) {
+      const finalNumber = wasClickedOrder.reduce(reducer);
+      console.log(wasClickedOrder.length);
+      setDisplayNumber(finalNumber);
+    }
     console.log("wasClickedOrder===>", wasClickedOrder);
   }, [wasClickedOrder]);
+
   const clickKeyboard = (ev) => {
     const clickedNumber = ev.target.innerText;
-    console.log(clickedNumber);
-    //const keyboardNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,.];
+
     if (clickedNumber === "X") {
       setWasClickedOrder((currentArray) => {
         return currentArray.slice(0, currentArray.length - 1);
       });
     } else {
-      setWasClickedOrder((currentArray) => currentArray.concat(clickedNumber));
+      console.log(wasClickedOrder.indexOf("."));
+      if (
+        wasClickedOrder.includes(".") &&
+        wasClickedOrder.length - wasClickedOrder.indexOf(".") > 2
+      ) {
+        console.log("Two digits after the decimal point are not counted :(");
+        return;
+      } else {
+        setWasClickedOrder((currentArray) =>
+          currentArray.concat(clickedNumber)
+        );
+      }
     }
-
-    console.log(displayNumber);
   };
   return (
     <div>
